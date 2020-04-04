de76308ac61928f1d366fda73dd4714ff94c505c
Maurice MJ de Jong <mauricemjdejong@LPTPMDJ.local>
2020-04-04 22:45:57 +0200

    Initial Module and Function

---
+++ src/Public/Test-ProductIsEnabled.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+    Tests if given product state has product state flag enabled
+.DESCRIPTION
+    Registry, WMI and other propties may contain a DWORD value or data object that represents the state of the corresponding product.
+    Specific enablement of the product is set to a bit in this DWORD, it can be challenging to gather the exact value of the flag.
+    This function will return true if the flag is set, meaning it is enabled. 
+.PARAMETER ProductState
+    The value (DWORD) containing the
+.EXAMPLE
+    PS C:\> Test-ProductIsEnabled -ProductState 393472
+    $false
+    This example shows basic functionality
+.OUTPUTS
+    Bool
+.NOTES
+    This function was build to resolve the state of a Antivirus Provider registered in Security Center. 
+    Using this function it is possible to read which provider is enabled or not.
+    Other states are Snoozed which can be tested with Test-ProductIsSnoozed
+#>
+function Test-ProductIsEnabled {
+    [CmdletBinding()]
+    param (
+        # Product State contains a value (DWORD) that contains multiple bitflags and we use the productState flag (0000F000)
+        [Parameter(Mandatory)]
+        [Int32]$ProductState
+    )
+    $ProductEnabled = 0x1000
+    $ProductStateMask = 0x0000f000
+    
+    try 
+    {
+        if( $($ProductEnabled -and $($ProductState -band $ProductStateMask) ) )
+        {
+            return $true
+        }
+        else 
+        {
+            return $false
+        }
+    }
+    catch 
+    {
+        return $false
+    }
+}
---
+++ src/helpers.psm1
@@ -0,0 +1,41 @@
+$Enums = @( Get-ChildItem -Path $PSScriptRoot\Enums\*.ps1 -ErrorAction SilentlyContinue )
+Foreach ($import in @($Enums)) {
+    Try {
+        Write-Verbose "Importing $($import.FullName)"
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+# Import Classes
+# if (Test-Path "$PSScriptRoot\Classes\classes.psd1") {
+#     $ClassLoadOrder = Import-PowerShellDataFile -Path "$PSScriptRoot\Classes\classes.psd1" -ErrorAction SilentlyContinue
+# }
+
+# foreach ($class in $ClassLoadOrder.order) {
+#     $path = '{0}\Classes\{1}.ps1' -f $PSScriptRoot, $class
+#     if (Test-Path $path) {
+#         . $path
+#     }
+# }
+
+# Get public and private function definition files.
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+# Dot source the files
+Foreach ($import in @($Public + $Private)) {
+    Try {
+        Write-Verbose "Importing $($Import.FullName)"
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import function $($import.fullname): $_"
+    }
+}
+
+Export-ModuleMember -Function $Public.Basename
+
+<# INSERT FOOTER BELOW #>
